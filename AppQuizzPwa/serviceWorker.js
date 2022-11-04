66ac3b006414abb89e0e55fefa675cd474f92da8
Maxime Préard <maxime.preard@hotmail.fr>
2022-11-04 14:50:04 +0100

    add pwa

--- AppQuizzPwa/serviceWorker.js
+++ AppQuizzPwa/serviceWorker.js
@@ -1,5 +1,5 @@
-const staticDevCoffee = "dev-coffee-site-v1"
-const assets = [
+const CACHE_NAME = "image-quiz-site-v1"
+const urlsToCache = [
     "/",
     "/index.html",
     "/level.html",
@@ -12,18 +12,57 @@ const assets = [
     "./assets/images/quizz.png",
 ]
 
-self.addEventListener("install", installEvent => {
-    installEvent.waitUntil(
-        caches.open(staticDevCoffee).then(cache => {
-            cache.addAll(assets)
-        })
+self.addEventListener('install', evt => {
+    evt.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+
+                return cache.addAll(urlsToCache);
+            })
     )
+    console.log("service worker has been installed");
 })
 
-self.addEventListener("fetch", fetchEvent => {
-    fetchEvent.respondWith(
-        caches.match(fetchEvent.request).then(res => {
-            return res || fetch(fetchEvent.request)
+self.addEventListener('activate', evt => {
+    let cacheWhiteList = [CACHE_NAME];
+
+    evt.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if(cacheWhiteList.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName)
+                    }
+                })
+            )
         })
     )
+    console.log("service worker has been activated");
+})
+
+self.addEventListener('fetch', evt => {
+    evt.respondWith(
+        caches.match(evt.request)
+            .then(response => {
+                if (response) return response;
+
+                let fetchRequest = evt.request.clone();
+
+                return fetch(fetchRequest)
+                    .then(response => {
+                        if (!response || response.status !== 200 || response.type !== 'basic') return response;
+
+                        let responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(cache => {
+                                cache.put(evt.request, responseToCache)
+                            });
+
+                        return response;
+                    });
+            })
+    )
+    console.log("Ressource récupérée" + evt.request.url);
 })
